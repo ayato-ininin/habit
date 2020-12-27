7049c4aabde66b7c053ef012f3841018daf30054
ayato-ininin <sarai.a.1203408@gmail.com>
2020-12-27 18:28:23 +0900

    add

--- src/components/HeaderAuth.vue
+++ src/components/HeaderAuth.vue
@@ -6,7 +6,7 @@
        </div>
        <div class="right-header flex">
           <p @click="$router.push('/signup')">新規登録</p>
-a         <p @click="$router.push('/main')">ログイン</p>
+         <p @click="$router.push('/main')">ログイン</p>
        </div>
       </div>
     </div>
