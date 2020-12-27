bb5792f42db1dbc70482bac77de8fbd1d718fedc
ayato-ininin <sarai.a.1203408@gmail.com>
2020-12-27 16:19:14 +0900

    ad

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -31,7 +31,7 @@ export default {
   },
   methods:{
     send(){
-      axios.post('http://127.0.0.1:8000/api/register',{
+      axios.post('https://fathomless-springs-88074.herokuapp.com/api/register',{
         name:this.name,
         email:this.email,
         password:this.password
