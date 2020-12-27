8249fd1176fea847b59eac40b10f77b2e4fdc216
ayato-ininin <sarai.a.1203408@gmail.com>
2020-12-27 20:47:22 +0900

    ad

--- src/store/index.js
+++ src/store/index.js
@@ -33,7 +33,7 @@ export default new Vuex.Store({
         }
       );
       commit("auth", responseLogin.data.auth);
-      router.replace("/main");
+      router.push({name:"Main"});
     },
     logout({ commit }) {
       axios.post("http://127.0.0.1:8000/api/logout", {
