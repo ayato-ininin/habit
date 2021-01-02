936887acfce4eca5fb98fbc27e8011ddaaa30b67
ayato-ininin <sarai.a.1203408@gmail.com>
2021-01-03 08:46:22 +0900

    addd

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="profile">
+    <Header></Header>
+    <DesignCss2></DesignCss2>
     <h1>工事中</h1>
   </div>
 </template>
+
+<script>
+import Header from '../components/Header'
+import DesignCss2 from '../components/DesignCss2'
+export default {
+  components:{
+    Header,
+    DesignCss2
+  }
+}
+</script>
